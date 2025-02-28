e56b0173a04f31ee40e0ee0f4fced4934fcecd3d
GabrielEGonzalez <enriquegonzalez123467@gmail.com>
2025-02-28 00:55:46 -0500

    sistema usuario

--- public/index.php
+++ public/index.php
@@ -1,17 +1,20 @@
 <?php
 
-    require_once '../app/config/config.php';
-    require_once '../app/init.php';
-    require_once '../app/lib/core.php';
-    require_once '../app/controllers/ControllersPrueba.php';
+require_once '../app/config/config.php';
+require_once '../app/init.php';
+require_once '../app/lib/core.php';
+require_once '../app/controllers/ControllersPrueba.php';
 
-    $url = new Core();
-    echo ''. $url->getUrl();
+$url = new Core();
+echo '' . $url->getUrl();
 
-    $nuevaURL = explode(BASE_URL , $url->getUrl());
+$nuevaURL = explode('/', $url->getUrl());
 
-    echo '' . $nuevaUrl;
+echo '<pre>';
+print_r($nuevaURL); // Muestra las partes de la URL
+echo '</pre>';
 
-    /* if(){
-
-    } */
+if((BASE_URL .'public/'. $nuevaURL[4].'/') === BASE_URL .'public/home/'){
+    $nueva = new ControllersPrueba();
+    $nueva->index();
+}
